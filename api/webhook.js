4519be8f6287b75219796fd06fdd226226422998
hasrulatho3-Trader <hasrulatho3@gmail.com>
2026-02-16 20:56:41 +0800

    add webhook api

---
+++ api/webhook.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const data = req.body;
+
+  console.log("Signal masuk:", data);
+
+  return res.status(200).json({ success: true });
+}
